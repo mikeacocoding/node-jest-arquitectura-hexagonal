1bd1d3990b69cc6e48fbd5fd8426ccc55716d8db
Andrés Pérez Zuluaga - Ceiba Software <andres.zuluaga@ceiba.com.co>
2020-03-10 12:12:56 -0500

    Añade logger a filtro de excepciones de dominio

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FiltroExcepcionesDeNegocio } from './infraestructura/excepciones/filtro-excepciones-negocio';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new FiltroExcepcionesDeNegocio());
+  const logger = app.get(Logger);
+
+  app.useGlobalFilters(new FiltroExcepcionesDeNegocio(logger));
+
   await app.listen(3000);
 }
 bootstrap();
